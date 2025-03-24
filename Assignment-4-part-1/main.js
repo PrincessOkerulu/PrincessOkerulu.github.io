2942b01d0a8bcbee4cd2de8e1da405427f9b6f38
Princess Okerulu <princess.okerulu@dcmail.ca>
2025-03-24 14:37:21 -0400

    Stored the strings inside the Arrays

--- Assignment-4-part-1/main.js
+++ Assignment-4-part-1/main.js
@@ -6,3 +6,26 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
+const insertY = ["the soup kitchen", "Disneyland", "the white House",]
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent =
+  story.style.visibility = 'visible';
+}
